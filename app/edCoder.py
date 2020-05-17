8520d247e0023fd18ff2901968ad72f796d92891
Mike Miller <mmiller@levelsbeyond.com>
2020-05-16 21:37:47 -0600

    fix

--- app/edCoder.py
+++ app/edCoder.py
@@ -1,14 +1,14 @@
 from math import floor
-import string
+from string import ascii_lowercase, ascii_uppercase, digits
 
 
 # Base62 Encoder and Decoder
 def toBase62(num, b=62):
     if b <= 0 or b > 62:
         return 0
-    base = string.digits + string.ascii_lowercase + string.ascii_uppercase
+    base = digits + ascii_lowercase + ascii_uppercase
     r = num % b
-    res = base[r];
+    res = base[r]
     q = floor(num / b)
     while q:
         r = q % b
@@ -18,7 +18,7 @@ def toBase62(num, b=62):
 
 
 def toBase10(num, b=62):
-    base = string.digits + string.ascii_lowercase + string.ascii_uppercase
+    base = digits + ascii_lowercase + ascii_uppercase
     limit = len(num)
     res = 0
     for i in range(limit):
